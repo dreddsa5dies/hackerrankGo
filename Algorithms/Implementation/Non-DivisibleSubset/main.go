b562f66a572a2a8afbab24b4751505f0830c5d81
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2016-10-28 17:19:23 +0300

    я не понимаю

--- Algorithms/Implementation/Non-DivisibleSubset/main.go
+++ Algorithms/Implementation/Non-DivisibleSubset/main.go
@@ -15,55 +15,18 @@ func main() {
 		arr = append(arr, v)
 	}
 
-	rem := []int{}
-	for j := 0; j < n; j++ {
-		rem[j] = arr[j] % k
-	}
+	arrC := map[int]int{}
 
-	ss := []int{}
-	ic, jc, zi, zj := 0, 0, 0, 0
-	for i := 0; i < n-1; i++ {
-		for j := i + 1; j < n; j++ {
-			if (rem[i] + rem[j]) == k {
-				ic = 0
-				jc = 0
-				for f := 0; f < n; f++ {
-					if rem[f] == i {
-						rem[f] = -1
-						ic++
-					} else if rem[f] == j {
-						rem[f] = -1
-						jc++
-					}
-				}
-				if ic >= jc {
-					for ic > 0 {
-						zi = 0
-						ss[zi] = i
-						zi++
-						ic--
-					}
-				} else {
-					for jc > 0 {
-						zj = 0
-						ss[zj] = j
-						zj++
-						jc--
-					}
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr); j++ {
+			if i != j {
+				if (arr[i]%k + arr[j]%k) == k {
+					arrC[arr[i]]++
+					arrC[arr[j]]++
 				}
 			}
 		}
 	}
-	temp := n
-	for r := 0; r < n; r++ {
-		if rem[r] >= 0 {
-			ss[temp-1] = rem[r]
-		}
-	}
-	newcount := 0
-	for nc := 0; nc < len(ss); nc++ {
-		newcount++
-	}
 
-	fmt.Println(newcount)
+	fmt.Println(arrC)
 }
