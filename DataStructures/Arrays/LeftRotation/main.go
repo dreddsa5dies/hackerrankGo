ebe3880551f12861ae0b39bdcc4c3916a9cd0631
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2020-05-02 19:34:39 +0300

    lest rotation OK

--- DataStructures/Arrays/LeftRotation/main.go
+++ DataStructures/Arrays/LeftRotation/main.go
@@ -2,53 +2,34 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
-	// stdin: uint64 - for all testcase
-	// get stdin
 	var (
-		n, a, d uint64
+		n, a, d int
 	)
 
 	fmt.Scanf("%d %d", &n, &d)
 
-	arr := []uint64{}
+	arr := []int{}
 
-	var i uint64
-
-	for i = 0; i < n; i++ {
+	for i := 0; i < n; i++ {
 		fmt.Scan(&a)
 		arr = append(arr, a)
 	}
 
-	// its algorithm
-	for i = 0; i < d; i++ {
-		arr = append(arr, arr[i])
+	for _, v := range leftRotation(arr, len(arr), d) {
+		fmt.Printf("%v ", v)
 	}
-
-	// copy arr
-	tempArr := make([]uint64, len(arr)-int(d))
-	copy(tempArr, arr[d:])
-
-	pP(tempArr)
-
 }
 
-func pP(arr []uint64) {
-	// stdout
-	str := ""
+func leftRotation(a []int, size int, rotation int) []int {
 
-	for k, v := range arr {
-		if k == len(arr)-1 {
-			s := strconv.FormatUint(v, 16)
-			str += s
-		} else {
-			s := strconv.FormatUint(v, 16)
-			str += s + " "
-		}
+	var newArray []int
+	for i := 0; i < rotation; i++ {
+		newArray = a[1:size]
+		newArray = append(newArray, a[0])
+		a = newArray
 	}
-
-	fmt.Println(str)
+	return a
 }
